1362c8f2de7a072a40b2a0cfd8ca369c7f1f516b
royalcorsair <111253312+royalcorsair@users.noreply.github.com>
2023-04-27 21:43:32 +0100

    First draft complete.

--- dailyStocksScraper.py
+++ dailyStocksScraper.py
@@ -2,7 +2,7 @@
 # dailyStocksScraper.py - Logs the data of any stock details after stock symbols entered 
 # into the command line
 
-import requests, sys
+import requests, sys, csv, os
 from pathlib import Path
 from datetime import datetime
 
@@ -29,10 +29,9 @@ while True:
         print("That address is incorrect. Try again.")
         i += 1
 
-stocksFile = open(address / f"{datetime.today().strftime('%d_%m_%Y')}_stocks.txt", "w")
+stocksFile = open(address / f"{datetime.today().strftime('%d_%m_%Y')}_stocks.txt", "w+")
 stocksFile.write("\t\t", datetime)
 stocksFile.write("\n\n")
-stocksFile.write("\t\tOpen\tHigh\tLow\tAdj")
 
 for symbol in symbols:
     res = requests.get(f"https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1=1651067896&period2=1682603896&interval=1d&events=history&includeAdjustedClose=true")
@@ -40,4 +39,13 @@ for symbol in symbols:
     csvFile = open(f"{symbol}.csv", "wb")
     for chunk in res.iter_content(100000):
         csvFile.write(chunk)
-    
+    csvReader = csv.reader(csvFile)
+    csvData = list(csvReader)
+    stocksFile.write(f"{symbol} - \n")
+    stocksFile.write("\t".join(csvData[1]))
+    stocksFile.write("\t".join(csvData[-1]))
+    csvData.close()
+    os.remove(f"{symbol}.csv")
+
+stocksFile.close()
+print("Done.")
